ccde7ff1bb541b56b68e8120eac01970dd08913e
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-09-26 09:55:19 -0400

    read census data function

--- forecasttools_py/data.py
+++ forecasttools_py/data.py
@@ -1,3 +1,38 @@
 """
-Retrieves US 2020 Census data and reads in NHSN influenza and COVID hospitalization counts data for use in example forecasting.
+Retrieves US 2020 Census data,
+hospitalization count data, and
+example forecast data.
 """
+
+import os
+
+import polars as pl
+
+
+def get_census_data(
+    save_path: str = os.getcwd(),
+    save_as_csv: bool = False,
+    url: str = "https://www2.census.gov/geo/docs/reference/state.txt",
+):
+    """
+    Retrieves US 2020 Census data in a
+    three column polars dataframe.
+    """
+    nation = pl.DataFrame(
+        {
+            "location_code": ["US"],
+            "short_name": ["US"],
+            "long_name": ["United States"],
+        }
+    )
+    jurisdictions = pl.read_csv(url, separator="|").select(
+        [
+            pl.col("STATE").alias("location_code").cast(pl.Utf8),
+            pl.col("STUSAB").alias("short_name"),
+            pl.col("STATE_NAME").alias("long_name"),
+        ]
+    )
+    flusight_location_table = nation.vstack(jurisdictions)
+    if save_as_csv:
+        flusight_location_table.write_csv("flusight_location_table.csv")
+    return flusight_location_table
