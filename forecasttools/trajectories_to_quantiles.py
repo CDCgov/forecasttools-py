e3917271d76712f447c58f5d794aea013dae4d87
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-10-15 11:37:47 -0400

    add trajectories docstring

--- forecasttools/trajectories_to_quantiles.py
+++ forecasttools/trajectories_to_quantiles.py
@@ -1,3 +1,9 @@
+"""
+Take value column of a dataframe with
+epiweekly draws and converts to quantiles
+over specified quantile levels.
+"""
+
 import numpy as np
 import polars as pl
 
@@ -11,6 +17,41 @@ def trajectories_to_quantiles(
     quantile_level_name: str = "quantile_level",
     id_cols: list[str] = None,
 ) -> pl.DataFrame:
+    """
+    Converts the value columns of a polars
+    dataframe of epiweekly draws (trajectories)
+    to quantile values over specified quantile
+    levels.
+
+    trajectories
+        A polars dataframe of epiweekly draws for
+        a single jurisdiction.
+    quantiles
+        A list of quantiles for the output values.
+        Defaults to None.
+    timepoint_cols
+        A list of dataframe columns that
+        identifies unique timepoints. Defaults
+        to ["timepoint"].
+    value_col
+        The name of the column with the
+        trajectory values. For example, "hosp",
+        "cases", "values". Defaults to "value".
+    quantile_value_name
+        The name of the column containing the
+        outputted quantile values. Defaults to
+        "quantile_value".
+    quantile_level_name
+        The name of the column containing the
+        quantiles levels. Defaults to
+        "quantile_level".
+    id_cols
+        Dataframe columns to aggregate before
+        the value_col is mapped to quantile
+        levels. For example, if there are multiple
+        locations present, one might group by
+        ["location"]. Defaults to None.
+    """
     # set default quantiles
     if quantiles is None:
         quantiles = (
