1663060cbb57b254d76d1cb0e332dd7587cb23cd
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-10-17 12:41:48 -0400

    messy vignette for scoringutils; additional code testing for adding dates and actual data (test data) to arviz; the problem is that scoringutils needs the observed data for the forecast period, and if I can have a way to add this data to an idata object, then all that would remain is ensuring the dates align

---
+++ forecasttools/to_scoringutils.py
@@ -0,0 +1,51 @@
+"""
+Takes differently formatted forecast objects
+and converts them into a format ready for R's
+`scoringutils`.
+"""
+
+import arviz as az
+import polars as pl
+
+
+def from_flusight_to_scoringutils(
+    submission: pl.DataFrame, observations: list[float]
+) -> None:
+    pass
+
+
+def from_idata_to_scoringutils(
+    save_path: str,
+    idata: az.InferenceData,
+    observations: list[float],
+    idata_samp_param: str,
+    idata_obs_param: str,
+    overwrite: str = False,
+) -> None:
+    """
+    Take a InferenceData object generated
+    from NumPyro posterior and posterior
+    predictive samples along with observations
+    and generate a parquet file with the
+    correct columns for use in `scoringutils`.
+
+    Parameters
+    ----------
+    save_path
+        The save path for the generated
+        parquet file.
+    idata
+        An InferenceData object containing
+        a forecast made using NumPyro.
+    observations
+        An array of observations corresponding
+        to the
+    overwrite
+        Whether to overwrite an existing file
+        of the same name.
+
+    Returns
+    -------
+    None
+        A parquet file.
+    """
