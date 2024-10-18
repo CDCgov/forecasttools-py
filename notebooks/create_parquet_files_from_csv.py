71e7a378d73be36cbfb36fa3b42d682367678921
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-10-18 15:24:02 -0400

    remove csv; add correct parquet for example flusight submission

--- notebooks/create_parquet_files_from_csv.py
+++ notebooks/create_parquet_files_from_csv.py
@@ -6,16 +6,35 @@ parquet files.
 # %% IMPORT LIBRARIES
 
 import glob
+import os
+import subprocess
 
 import polars as pl
 
-# %%
+import forecasttools
+
+# %% CREATE PARQUET FILES
 
 csv_files = glob.glob("../forecasttools/*.csv")
 for csv in csv_files:
-    df_from_csv = pl.read_csv(csv, ignore_errors=True)
+    dtypes_d = {"location": pl.Utf8}
+    df_from_csv = pl.read_csv(csv, schema_overrides=dtypes_d)
     parquet_name = csv.replace("csv", "parquet")
-    df_from_csv.write_parquet(parquet_name)
+    if not os.path.exists(parquet_name):
+        df_from_csv.write_parquet(parquet_name)
+        print(f"New file created:\n{parquet_name}")
 
+
+# %% TEST FILES
+
+forecasttools.example_flusight_submission
+forecasttools.location_table
+forecasttools.nhsn_hosp_flu
+forecasttools.nhsn_hosp_COVID
+
+# %% TEST ALL GOOD
+
+for csv in csv_files:
+    subprocess.run(["rm", csv])
 
 # %%
