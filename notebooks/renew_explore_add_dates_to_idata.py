29f9ba4f65c6fe2f82f2e607ad99789c9714f18b
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-10-21 15:31:12 -0400

    update idata dates experimentation file

--- notebooks/renew_explore_add_dates_to_idata.py
+++ notebooks/renew_explore_add_dates_to_idata.py
@@ -8,20 +8,28 @@ import forecasttools
 
 xr.set_options(display_expand_data=False, display_expand_attrs=False)
 
+# accept either str or datetime
+# polars date col as datetime
+# change .draw to draw
+# generic reference wo/ string construction (.dim)
+# obs_dim_0 --> day_index
 
-# %% CREATING DATES ARRAY
+# %% SETTING UP START DATE AND IDATA
 
-# retrieve inference data object from forecasttools
-idata = forecasttools.nhsn_flu_forecast
-
-# create fictitious starting date for fitting
+# received by function is a str ISO8601 start date
 start_date_iso = "2022-08-01"
 start_date_as_dt = dt.datetime.strptime(start_date_iso, "%Y-%m-%d")
 
-# get the size of the fitting data
-obs_length = idata.observed_data["obs_dim_0"].size
+# and an inference data object (built az.from_numpyro())
+idata = forecasttools.nhsn_flu_forecast
 
-# create an array of dates corresponding to the fitting data
+# examine groups available with idata
+print(idata)
+
+# %% DATE CALCULATIONS FOR OBSERVED DATA
+
+# the length of the observed data becomes its date array
+obs_length = idata.observed_data["obs_dim_0"].size
 obs_dates = np.array(
     [
         (start_date_as_dt + dt.timedelta(days=i)).strftime("%Y-%m-%d")
@@ -29,19 +37,40 @@ obs_dates = np.array(
     ]
 )
 
+# %% DATE CALCULATIONS FOR POSTERIOR PREDICTIVE
+
+# the length of the posterior predictive data becomes its date array
+postp_length = idata.posterior_predictive["obs_dim_0"].size
+postp_dates = np.array(
+    [
+        (start_date_as_dt + dt.timedelta(days=i)).strftime("%Y-%m-%d")
+        for i in range(postp_length)
+    ]
+)
+
+
 # %% ADDING DATES TO IDATA
 
-# modify the inference data observed data with new dates
+# modify the idata group observed data with new dates
 idata.observed_data = idata.observed_data.assign_coords(obs_dim_0=obs_dates)
 
-# retrieve observed dates
-obs_dates = idata.observed_data.coords["obs_dim_0"].values
-print(obs_dates)
+# modify the idata group posterior predictive with new dates
+idata.posterior_predictive = idata.posterior_predictive.assign_coords(
+    obs_dim_0=postp_dates
+)
 
+# %% EXAMINE NEW DATED IDATA GROUPS
 
-# examine other properties
-obs_dates = idata.posterior_predictive.coords["obs_dim_0"].values
-print(obs_dates)
+# examine observed_data dates
+obs_dates_out = idata.observed_data.coords["obs_dim_0"].values
+print(f"Obs start should be: {start_date_iso}\n{obs_dates_out[:10]}")
+
+# examine posterior_predictive dates
+postp_dates_out = idata.posterior_predictive.coords["obs_dim_0"].values
+print(f"PostP start should be: {start_date_iso}\n{postp_dates_out[:10]}")
+print(
+    f"PostP size should be greater than obs group:\nPostP: {len(postp_dates_out)} v. Obs: {len(obs_dates_out)}, Diff: {len(postp_dates_out) - len(obs_dates_out)}"
+)
 
 
 # %%
