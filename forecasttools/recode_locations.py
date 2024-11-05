55f19cbdc267422487e4e2ba4f6679ffa8a5ad45
upx3—TM (CFA) <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-11-05 12:02:48 -0500

    Update forecasttools/recode_locations.py
    Co-authored-by: Dylan H. Morris <dylanhmorris@users.noreply.github.com>

--- forecasttools/recode_locations.py
+++ forecasttools/recode_locations.py
@@ -42,7 +42,7 @@ def loc_abbr_to_flusight_code(
     # recode and replaced existing loc abbrs
     # with loc codes
     loc_recoded_df = df.with_columns(
-        location=pl.col("location").replace(
+        pl.col(location_col).replace(
             old=loc_table["short_name"],
             new=loc_table["location_code"],
         )
