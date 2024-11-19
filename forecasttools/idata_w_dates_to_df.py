358b772b390b6c0010154ae055f77fec4b7db5fe
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-11-19 15:40:24 -0500

    remove {e}

--- forecasttools/idata_w_dates_to_df.py
+++ forecasttools/idata_w_dates_to_df.py
@@ -237,7 +237,7 @@ def add_time_coords_to_idata_dimensions(
             )
         except ValueError as e:
             raise ValueError(
-                f"Error for (group={group}, variable={variable}, dimension={dimension}): {e}"
+                f"Error for (group={group}, variable={variable}, dimension={dimension})."
             ) from e
 
     return idata
