cca8f02ced3998963b139296c2c47dbc28198268
Dylan H. Morris <dylanhmorris@users.noreply.github.com>
2025-01-27 23:35:30 +0000

    Fix hard-coded column names in `daily_to_epiweekly()` (#56)
    * start README update; start github action for rendering; render readme
    
    * update action checkout
    
    * test out README rendering
    
    * attempt fix action via add python setup
    
    * further test render README
    
    * further modification of readme rendering
    
    * display covid data
    
    * reorder workflow
    
    * another attempt at proper quarto poetry env
    
    * Auto-update README.md
    
    * minor change to README
    
    * attempt adoption of GVY recommendation
    
    * Auto-update README.md
    
    * attempt fix pre-commit with yaml; add to README
    
    * add polars
    
    * Auto-update README.md
    
    * clean README
    
    * Auto-update README.md
    
    * use kable for dataframes; test details block
    
    * Auto-update README.md
    
    * attempt use of asis; remove kable; remove xarry extended formatting
    
    * Auto-update README.md
    
    * attempt w/ print statements over quarto formatting
    
    * Auto-update README.md
    
    * remove output asis; comment polars formatting; add print statements
    
    * Auto-update README.md
    
    * final experiment with the details block
    
    * Auto-update README.md
    
    * remove details attempt; PR ready
    
    * Auto-update README.md
    
    * minor workflow language change
    
    * tentative utilities diagram
    
    * Render README.qmd To README.md
    
    * remove quarto executable
    
    * Render README.qmd To README.md
    
    * close details
    
    * Render README.qmd To README.md
    
    * move render readme yaml
    
    * update pre-commit config and workflow
    
    * add pre-commit hook; test README via updating
    
    * attempt fix pre-commit workflow
    
    * attempt fix pre-commit workflow, again
    
    * try wo/ quarto env path
    
    * changes to script; change to config; update README
    
    * slight script mod
    
    * slight script mod
    
    * add exit codes
    
    * change re-commit exit code
    
    * exit code test
    
    * remove quiet flag
    
    * update README.md for rendering
    
    * remove comments; add space ignore
    
    * test comment; add no cache
    
    * quarto render to gfm
    
    * updated quarto
    
    * add back comments; remove explicit --to gfm
    
    * add space to rand file, see speed
    
    * set run always to true
    
    * minor edits to flags and hook
    
    * Update .pre-commit-config.yaml
    
    * Rebase remote
    
    * Update pre-commit.yaml
    
    Need to poetry install explicitly or risk of hook failure after pre-commit-config change
    
    * Render with latest quarto
    
    * Remove poetry install call
    
    * Do not run poetrty install within hook
    
    * Run poetry install outside of hook
    
    * poetry install in hook
    
    * revert to no install in hook
    
    * Try with files: defined
    
    * Fix hard-coded column names in daily_to_epiweekly
    
    ---------
    
    Co-authored-by: AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- forecasttools/daily_to_epiweekly.py
+++ forecasttools/daily_to_epiweekly.py
@@ -8,6 +8,8 @@ from datetime import datetime
 import epiweeks
 import polars as pl
 
+from forecasttools.utils import ensure_listlike
+
 
 def calculate_epi_week_and_year(date: str):
     """
@@ -26,46 +28,43 @@ def calculate_epi_week_and_year(date: str):
 
 
 def df_aggregate_to_epiweekly(
-    forecast_df: pl.DataFrame,
+    df: pl.DataFrame,
     value_col: str = "value",
     date_col: str = "date",
-    id_cols: list[str] = ["draw"],
+    id_cols: str | list[str] = ["draw"],
     weekly_value_name: str = "weekly_value",
-    strict: bool = False,
+    strict: bool = True,
 ) -> pl.DataFrame:
     """
-    Aggregate daily values (e.g.
+    Aggregate a dataframe of daily values (e.g.
     hospitalizations) to epiweekly total values
     and add epiweek and epiyear columns.
 
     Parameters
     ----------
-    forecast_df
-        A polars dataframe with draws and dates
-        as columns. This dataframe will likely
-        have come from an InferenceData object
-        that was converted using `idata_w_dates_to_df`.
+    df
+        Tidy data frame of daily values to aggregate.
     value_col
-        The name of the column with the fitted
-        and or forecasted quantity. Defaults
-        to "value".
+        The name of the column containing daily trajectory /
+        timeseries values to aggregate. Defaults
+        to ```"value"``.
     date_col
         The name of the column with dates.
-        Defaults to "date".
+        Defaults to ``"date"``.
     id_cols
         The name(s) of the column(s) that
         uniquely identify a single timeseries
         (e.g. a single posterior trajectory).
-        Defaults to [".draw"].
+        Defaults to ``"draw"``.
     weekly_value_name
         The name to use for the output column
         containing weekly trajectory values.
-        Defaults to "weekly_value".
+        Defaults to ``"weekly_value"``.
     strict
         Whether to aggregate to epiweekly only
         for weeks in which all seven days have
-        values. If False, then incomplete weeks
-        will be aggregated. Defaults to False.
+        values. If ``False``, then incomplete weeks
+        will be aggregated. Defaults to ``True``.
 
     Returns
     -------
@@ -73,9 +72,10 @@ def df_aggregate_to_epiweekly(
         A dataframe with value_col aggregated
         across epiweek and epiyear.
     """
+    id_cols = ensure_listlike(id_cols)
     # add epiweek and epiyear columns
-    forecast_df = forecast_df.with_columns(
-        pl.col(["date"])
+    df = df.with_columns(
+        pl.col(date_col)
         .map_elements(
             lambda elt: calculate_epi_week_and_year(elt),
             return_dtype=pl.Struct,
@@ -84,32 +84,29 @@ def df_aggregate_to_epiweekly(
     ).unnest("epi_struct_out")
     # group by epiweek, epiyear, and the id_cols
     group_cols = ["epiweek", "epiyear"] + id_cols
-    grouped_df = forecast_df.group_by(group_cols)
+    grouped_df = df.group_by(group_cols)
     # number of elements per group
     n_elements = grouped_df.agg(pl.count().alias("n_elements"))
     problematic_trajectories = n_elements.filter(pl.col("n_elements") > 7)
     if not problematic_trajectories.is_empty():
-        message = f"Problematic trajectories with more than 7 values per epiweek per year: {problematic_trajectories}"
         raise ValueError(
-            f"At least one trajectory has more than 7 values for a given epiweek of a given year.\n{message}"
+            "At least one trajectory has more than 7 values "
+            "for a given epiweek of a given epiyear.\n"
+            "Problematic trajectories with more than 7 "
+            "values: "
+            f"{problematic_trajectories}"
         )
-    # check if any week has more than 7 dates
-    if not n_elements["n_elements"].to_numpy().max() <= 7:
-        raise ValueError(
-            "At least one trajectory has more than 7 values for a given epiweek of a given year."
-        )
-    # if strict, filter out groups that do not have exactly 7 contributing dates
+
     if strict:
         valid_groups = n_elements.filter(pl.col("n_elements") == 7)
-        forecast_df = forecast_df.join(
+        df = df.join(
             valid_groups.select(group_cols),
             on=group_cols,
             how="inner",
         )
-    # aggregate; sum values in the specified value_col
     df = (
-        forecast_df.group_by(group_cols)
+        df.group_by(group_cols)
         .agg(pl.col(value_col).sum().alias(weekly_value_name))
-        .sort(["epiyear", "epiweek", "draw"])
+        .sort(group_cols)
     )
     return df
